27550d26ade5b784ec5e6fad32ebbea69fee42ca
griseldagomez <grinoe123@gmail.com>
2024-07-16 10:34:06 +0200

    starting with js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
 'use strict';
 
+//Pasos en humano
+
+// Primera parte (Buscar)
+
+// - Crear la estructura donde la usuaria buscara la serie.
+// - Saber la cantidad de series que tendrá la persona para elegir.
+// - Saber cuando la usuaria hace click en Buscar la Serie.
+// - Recoger los datos de la elección de la usuaria.
+// - Mostrar en el DOM la elección de la usuaria.
+
+// Segunda parte (Favoritos)
+
+// - La persona podrá elegir como favoritas cualquiera de las opciones de series.
+// -Se cambiaran el color de fondo  y de fuente de la serie seleccionada.
+// - Mostrar la elección favorita de la usuaria en la parte izquierda de la pantalla debajo del formulario de búsqueda.
+// - La serie favorita seguirá mostrándose en el DOM aunque la usuaria realice otra búsqueda.
+
+// Tercera parte (Almacenamiento local)
+
+// -Guardar las selecciones favoritas de la usuaria en el navegador local.
+// -Al recargar la pagina se mostrará el listado de favoritos.
+
+
+const input = document.querySelector('.js-input');
+const button = document.querySelector('js.button');
+
+
